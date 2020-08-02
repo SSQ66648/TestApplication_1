edc945f857a7ed0da06e580a1c40b44aade0ceba
TC <ssq16shu@uea.ac.uk>
2020-08-02 19:16:41 +0100

    simple unit tests for CalculatorClass complete and successfully passed

--- TestProject_1.Unit.Test/UnitTest1.cs
+++ TestProject_1.Unit.Test/UnitTest1.cs
@@ -10,10 +10,9 @@
  *  + TestProject_1 has been added as a project reference to access namespace.
  *  + uses "AAA" pattern for layout (Arrange, Act, Assert)
  * To Do:
- *  unit test for:
- *  + subtraction
- *  + multiplication
- *  + division
+ *  +
+ * Lesser To Do:
+ *  + check if variables can be global or must be repeated locally?
  *******************************************************************************/
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TestProject_1;
@@ -28,12 +27,60 @@ namespace TestProject_1.Unit.Test
         {
             //Arrange
             var calculator = new CalculatorClass();
-            int firstNumber = 8;
-            int secondnumber = 6;
+            int firstNumber = 6;
+            int secondnumber = 8;
             int expected = 14;
 
             //Act
             int actual = calculator.Add(firstNumber, secondnumber);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Subtract()
+        {
+            //Arrange
+            var calculator = new CalculatorClass();
+            int firstNumber = 8;
+            int secondnumber = 6;
+            int expected = 2;
+
+            //Act
+            int actual = calculator.Subtract(firstNumber, secondnumber);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Multiply()
+        {
+            //Arrange
+            var calculator = new CalculatorClass();
+            int firstNumber = 6;
+            int secondnumber = 8;
+            int expected = 48;
+
+            //Act
+            int actual = calculator.Multiply(firstNumber, secondnumber);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Divide()
+        {
+            //Arrange
+            var calculator = new CalculatorClass();
+            int firstNumber = 30;
+            int secondnumber = 10;
+            int expected = 3;
+
+            //Act
+            int actual = calculator.Divide(firstNumber, secondnumber);
 
             //Assert
             Assert.AreEqual(expected, actual);
