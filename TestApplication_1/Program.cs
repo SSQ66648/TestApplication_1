e85f3339ff9bee91b90cedb22a6b6cd46673c301
TC <ssq16shu@uea.ac.uk>
2020-08-02 15:32:10 +0100

    removed previous calculator class to focus on learning the Visual Studio hierarchy (terminology eg  solution, project etc) -renamed solution and project accordingly to emphasise this -reverted to hello world content

--- TestApplication_1/Program.cs
+++ TestApplication_1/Program.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/********************************************************************************
+ * Solution:    TestSolution_1.sln
+ * Project:     TestProject_1.csproj
+ * File:        program.cs
+ * Author:      SSQ66648
+ * Description: 
+ *      Entry point for test application: 
+ *      Used for experimentation while learning how to operate Visual Studio.
+ *      Includes basic-arithemetic calculator class for testing unit-tests.
+ * Notes:
+ *  +   Solution and project renamed from "TestApplication_1" for hierarchical clarity.
+ * ToDo:
+ *  +   Add calculator class
+ *  +   Create unit test for calculator class
+ *******************************************************************************/
+
+
+using System;
 
 namespace TestApplication_1
 {
@@ -7,6 +24,8 @@ namespace TestApplication_1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
         }
     }
 }
